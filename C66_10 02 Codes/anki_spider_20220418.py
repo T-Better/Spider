c1463c7d59310156c9d0bd0544b07d66a16a3404
claren <www.1121008136@foxmail.com>
2022-04-18 14:25:39 +0800

    20220418 复习之前学过的爬虫知识

---
+++ C66_10 02 Codes/anki_spider_20220418.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+res = '''
+<html lang="en">
+    <body>
+        <h1 class="title">华能信托是家好公司</h1>
+        <h1 class="title">上海交大是所好学校</h1>
+        <a href="https://www.baidu.com/" id="source">百度新闻</a>
+    </body>
+</html>
+'''
+soup1 = BeautifulSoup(res,'html.parser')
+r1 = soup1.select('.title')
+for i in r1:
+    print(i.text)
+
+res2 = '''
+<html lang="en">
+    <body>
+        <h1 class="title">华能信托是家好公司</h1>
+        <h1 class="title">上海交大是所好学校</h1>
+        <a href="https://www.baidu.com/" id="source">百度新闻</a>
+    </body>
+</html>
+'''
+
+
