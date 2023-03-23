ca76d7a882ac8089bcd2824665a089922caf8504
claren <www.1121008136@foxmail.com>
2023-03-23 09:16:21 +0800

    20230323 整理

--- C66_10 02 Codes/2022/用selenium库获取网页源代码_20220401.py
+++ C66_10 02 Codes/2022/用selenium库获取网页源代码_20220401.py
@@ -4,7 +4,7 @@ browser = webdriver.Chrome()
 url_finance = r'https://finance.sina.com.cn/realstock/company/sh000001/nc.shtml'
 browser.get(url_finance)
 data = browser.page_source  # 获取源码
-local_path_finance = r'D:\Git_Reps\SoftTest\Spider_Man\Spider\C66_10 02 Codes\上证指数sourcepage_20220401.txt'
+local_path_finance = r'/SoftTest/Spider_Man/Spider/C66_10 02 Codes/2022/上证指数sourcepage_20220401.txt'
 with open(local_path_finance, 'w+',encoding='utf-8') as f:
     f.write(data)
 browser.close()
--- C66_10 02 Codes/2023/03月/20230316/爬百度搜索python的结果.py
+++ C66_10 02 Codes/2023/03月/20230316/爬百度搜索python的结果.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = r"https://www.baidu.com/s"
     kv = {'wd': 'python'}
     bdata = parse(url, kv)
-    with open('./baidu.txt',mode='w',encoding='UTF8') as f:
+    with open('baidu.txt', mode='w', encoding='UTF8') as f:
         f.write(bdata)
 
 
