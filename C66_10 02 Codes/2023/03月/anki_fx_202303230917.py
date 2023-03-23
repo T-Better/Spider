3a36db917c77d59f7d035b3f324c7fa8166a034e
claren <www.1121008136@foxmail.com>
2023-03-23 19:45:37 +0800

    20230323

---
+++ C66_10 02 Codes/2023/03月/anki_fx_202303230917.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+
+res1 = '''
+<html lang="en">
+    <body>
+        <h1 class="title">华能信托是家好公司</h1>
+        <h1 class="title">上海交大是所好学校</h1>
+        <a href="https://www.baidu.com/" id="source">百度新闻</a>
+    </body>
+</html>
+'''
+
+s1 = BeautifulSoup(res1, 'html.parser')
+r = s1.select('.result1 h1')
+for i in r:
+    print(r.text)
+
+
+
+
+
+
+
+
+
