02db039345e8e17aedf48e165ec5a2729b19ecaf
claren <www.1121008136@foxmail.com>
2023-04-16 15:47:27 +0800

    20230416 anki复习

---
+++ C66_10 02 Codes/2023/04月/anki_spider_20230416.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+res = '''
+<html lang="en">
+    <body>
+        <h1 class="title">华能信托是家好公司</h1>
+        <h1 class="title">上海交大是所好学校</h1>
+        <a href="https://www.baidu.com/" id="source">百度新闻</a>
+    </body>
+</html>
+'''
+s1 = BeautifulSoup(res,'html.parser')
+r = s1.select('#source')
+
+for i in r:
+    print(i['href'])
