19de813174f05cfb2c16a04de7eb5372dccbdaac
claren <www.1121008136@foxmail.com>
2023-05-15 19:18:02 +0800

    20230515 自动化测试所有设计内容均已学完 开始产出

---
+++ C66_10 02 Codes/2023/05月/anki_20230515.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+res = '''
+<html lang="en">
+    <body>
+        <h1 class="title">华能信托是家好公司</h1>
+        <h1 class="title">上海交大是所好学校</h1>
+        <a href="https://www.baidu.com/" id="source">百度新闻</a>
+    </body>
+</html>
+'''
+s1 = BeautifulSoup(res,'html.parser')
+r1 = s1.select('#source')
+for i in r1:
+    print(i['href'])
+
+
+
+
+
+
+
+
+
+
+
