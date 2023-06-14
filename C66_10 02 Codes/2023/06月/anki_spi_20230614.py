0d663bf01bbb2cb1cc6e14a2407197c840b6673c
claren <www.1121008136@foxmail.com>
2023-06-14 10:09:56 +0800

    20230614 复习 over

---
+++ C66_10 02 Codes/2023/06月/anki_spi_20230614.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+
+
+res = '''
+<html lang="en">
+    <body>
+        <h1 class="title">华能信托是家好公司</h1>
+        <h1 class="title">上海交大是所好学校</h1>
+        <a href="https://www.baidu.com/" id="source">百度新闻</a>
+    </body>
+</html>
+'''
+soup1 = BeautifulSoup(res,'html.parser')
+r = soup1.select('#source')
+for i in r:
+    print(i.attrs['href'])
+
+
+
+
